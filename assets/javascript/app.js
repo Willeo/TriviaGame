03659c9148fda57eaf0ad53f5894f944839f0c85
Willeo <willeo.smith@outlook.com>
2018-07-02 20:44:25 -0500

    random question selection work. Displaying to the screen.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,19 @@
 // document ready block 
 $(document).ready(function(){
 
-var myList = ["who", "what", "when", "why", "how"];
+
 var correctAnswer = 0;
 var incorrectAnswer = 0;
 var stopTime = clearTimeout();
 var count=10;
 var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
 
-
-
-var rand = mylist[Math.floor(Math.random() * myList.length)];
-
+var myList = ["who wrote this?", "what is this?", "when did you write this?", "why", "how"];
+var rand = myList[Math.floor(Math.random() * myList.length)];
 
 $("#whens").append(rand);
-
-
 function timer() {
+
   count=count-1;
   $("#timer").html(count);
   if (count <= 0) {
