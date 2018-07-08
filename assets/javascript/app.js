614d80803ce5e7e8ec40bfff99b385764a17b43b
Willeo <willeo.smith@outlook.com>
2018-07-07 23:09:42 -0500

    added sudo code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,36 @@
-// document ready block 
-$(document).ready(function(){
+// document ready block
+$(document).ready(function() {
+  // add start / reset function
+  var userPick = null;
+  var correctAnswer = 0;
+  var incorrectAnswer = 0;
+  var stopTime = clearTimeout();
+  var count = 10;
+  var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
 
+  var myList = [
+    "who wrote this?",
+    "what is this?",
+    "when did you write this?",
+    "why",
+    "how"
+  ];
+  var rand = myList[Math.floor(Math.random() * myList.length)];
 
-var correctAnswer = 0;
-var incorrectAnswer = 0;
-var stopTime = clearTimeout();
-var count=10;
-var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+  // logic for the selection
+  $("#whens").append(rand);
 
-var myList = ["who wrote this?", "what is this?", "when did you write this?", "why", "how"];
-var rand = myList[Math.floor(Math.random() * myList.length)];
-
-$("#whens").append(rand);
-function timer() {
-
-  count=count-1;
-  $("#timer").html(count);
-  if (count <= 0) {
-     clearInterval(counter);
-     //counter ended, Times up      
-     $("#results").html("Times up!");
-     return;
-  }
-  console.log(count);  
-  //Do code for showing the number of seconds here
-}// end timer function
-
+  // timer function works
+  function timer() {
+    count = count - 1;
+    $("#timer").html(count);
+    if (count <= 0) {
+      clearInterval(counter);
+      //counter ended, Times up
+      $("#results").html("Times up!");
+      return;
+    }
+    console.log(count);
+    //Do code for showing the number of seconds here
+  } // end timer function
 }); // end document ready
